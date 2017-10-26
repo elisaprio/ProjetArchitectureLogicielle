4f45cd7880b516ca5eb3da9b85567fd21c25c3a8
tuveuxuncoca <adl.rlc95@gmail.com>
2017-10-26 18:06:43 +0200

    Test2

--- ProjetFiles_Belloeil_Priollaud_Rault/src/Etape1/CasFiles.java
+++ ProjetFiles_Belloeil_Priollaud_Rault/src/Etape1/CasFiles.java
@@ -5,17 +5,25 @@ import java.util.LinkedList;
 public class CasFiles<T> implements Files<T>{
 	private LinkedList<T> file = new LinkedList<T>();
 	
+	// Constructeur creant une liste vide
+	
 	public CasFiles(){
 		this.file= new LinkedList<T>();
 	}
+	
+	// Constructeur prenant en argument une liste
 	
 	public CasFiles(LinkedList<T> file){
 		this.file=file;
 	}
 	
+	// Méthode retournant True si la liste est vide, False sinon
+	
 	public boolean isEmpty(){
-		return true;
+		return this.file.isEmpty();
 	}
+	
+	// Méthode retournant le dernier élément de la liste
 	
 	public T dernierElement(){
 		return this.file.getLast();
