e99ed99e4f90941e4c42ebe1d341e1d15b77aaa4
VinceBEL <vince-om-91@hotmail.fr>
2017-11-07 18:41:43 +0100

    iterateur

--- ProjetFiles_Belloeil_Priollaud_Rault/src/tp/v2/IterateurListe.java
+++ ProjetFiles_Belloeil_Priollaud_Rault/src/tp/v2/IterateurListe.java
@@ -5,20 +5,30 @@ import java.util.Iterator;
 public class IterateurListe<E> implements Iterator<E> {
 
 	private Liste liste;
+	private int index;
 	
 	public IterateurListe(Liste liste){
 		this.liste=liste;
+		this.index=0;
 	}
 	
 	@Override
 	public boolean hasNext() {
 		// TODO Auto-generated method stub
-		return false;
+		if (this.index<this.liste.taille()){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	@Override
 	public E next() {
 		// TODO Auto-generated method stub
+		if(this.hasNext()){
+			index++;
+		}
 		return null;
 	}
 
