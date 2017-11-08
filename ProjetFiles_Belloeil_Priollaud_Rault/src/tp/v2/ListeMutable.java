564fd1b2cc2c039fb77b982b6a82fe4126bd8469
VinceBEL <vince-om-91@hotmail.fr>
2017-11-08 17:37:27 +0100

    Liste Mutable avec imlémentations

--- ProjetFiles_Belloeil_Priollaud_Rault/src/tp/v2/ListeMutable.java
+++ ProjetFiles_Belloeil_Priollaud_Rault/src/tp/v2/ListeMutable.java
@@ -27,13 +27,28 @@ public interface ListeMutable<E> extends Liste<E>{
 
 	public static <E> ListeMutable<E> cons(E t, ListeMutable<E> r){
 		return new ListeMutable<E>() {
-			// TODO Définir les méthodes utiles.			
+			// TODO Définir les méthodes utiles.	
+			public boolean casCons(){
+				return true;
+			}
+			public E tete(){
+				return t;
+			}
+			public ListeMutable<E> reste(){
+				return r;
+			}
+			public int taille(){
+				return 1+r.taille();
+			}
 		};
 	}
 	
 	public static <E> ListeMutable<E> vide() {
 		return new ListeMutable<E>() {
-			// TODO Définir les méthodes utiles.						
+			// TODO Définir les méthodes utiles.	
+			public boolean casVide(){
+				return true;
+			}
 		};
 	}
 	
